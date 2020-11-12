8aa277799af68c0f9c74122af6d712f06eda1aa3
Onion <20182005093@m.scnu.edu.cn>
2020-11-12 16:28:03 +0800

    完善了dao类

--- app/src/test/java/daotest.java
+++ app/src/test/java/daotest.java
@@ -1,12 +1,11 @@
 import onion.bookapp.DB.DAO.GoodsDAO;
-import onion.bookapp.DB.DBFactory;
-import onion.bookapp.DB.DBUtils;
-import onion.bookapp.DB.Impl.GoodsDAOImpl;
+import onion.bookapp.DB.Impl.GoodsDAOProxy;
 import onion.bookapp.mybean.data.Goods;
 
 public class daotest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Goods goods=new Goods("goodsidtest","publisheridtest","imagestest","publishTimetest","titletest","detailtest","sorttest","pricetest");
-        DBFactory.getInstance().insert(goods);
+        GoodsDAO dao= new GoodsDAOProxy();
+        dao.insert(goods);
     }
 }
